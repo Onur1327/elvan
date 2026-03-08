178bdd1a57528bde874d76ed3af0274dce05641d
Onur1327 <onurlacin1995@gmail.com>
2026-03-08 14:18:44 +0300

    chore: remove unused django backend and virtual environment

--- backend/store/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import StoreInfo
-
-class StoreInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StoreInfo
-        fields = '__all__'
--- backend/store/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import StoreInfoAPIView
-
-urlpatterns = [
-    path('store-info/', StoreInfoAPIView.as_view(), name='store-info'),
-]
